e48889a189eeaa9ff195e628af615d611dacd959
Eva <y_davydova@kbtu.com>
2025-10-23 19:29:35 +0500

    Moderate header section

--- my-app/src/app/header/header.ts
+++ my-app/src/app/header/header.ts
@@ -11,5 +11,11 @@ import { CommonModule } from '@angular/common';
 export class Header {
   title = 'Art Institue of Chicago';
   schedule = 'Daily 10-8';
+  message = ''
   photoUrl = "https://artic-web.imgix.net/905abd91-5c0d-451b-9319-f7cd1505bc33/IM026911_002-web.jpg?w=1200&h=800&fit=crop"
+
+  toggleMessage() {
+    this.message = this.message ? '' : 'Pablo Picasso "Paragraphs". 1.11 - 15.11';
+  }
+
 }
