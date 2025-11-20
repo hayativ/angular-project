8343ce645a347fc60828500945cbb3dbd2e7dbb6
Eva <y_davydova@kbtu.com>
2025-11-21 00:14:43 +0500

    Install Firebase

---
+++ my-app/src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebase:{
+        apiKey: "AIzaSyB77nU0nfLgFDjoG6VJUOzF45kbc-LaXgM",
+        authDomain: "art-institute-angular.firebaseapp.com",
+        projectId: "art-institute-angular",
+        storageBucket: "art-institute-angular.firebasestorage.app",
+        messagingSenderId: "876460382670",
+        appId: "1:876460382670:web:b7c121bd1f28986277739f"
+    },
+};
